7c497288217aec923ca35a968bad3ee392947f41
Sarah Garlock <garlock.sarah10@gmail.com>
2023-02-08 18:25:35 -0700

    Add places

--- friends.rb
+++ friends.rb
@@ -1,7 +1,13 @@
-arrays = ["Josh", "John", "Jude", "Jess", "Johan"]
+# arrays = ["Josh", "John", "Jude", "Jess", "Johan"]
 
-puts arrays[2]
+# puts arrays[2]
 
-puts arrays[-1]
+# puts arrays[-1]
 
-puts arrays[0]
+# puts arrays[0]
+
+places = ["Italy", "Mexico", "Peru", "Germany", "Japan"]
+
+puts places.pop
+
+puts places.push("China")
